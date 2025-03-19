f797c2b26981a65593ec6787414b4799c7fd14ed
Kobe Vongsady <kobevongsady@gmail.com>
2025-03-19 13:52:46 -0700

    something

---
+++ src/main/java/com/github/kobevongsady/calculator_new/Calculator.java
@@ -0,0 +1,18 @@
+package com.github.kobevongsady.calculator_new;
+
+    public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int divide(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot divide by zero");
+            }
+            return a / b;
+        }
+    }
